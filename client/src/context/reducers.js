ad63a89b37c1924698f24e576c513e20e3718705
oriebalzyuot96 <=>
2020-07-30 15:09:13 +0300

    90% done

--- client/src/context/reducers.js
+++ client/src/context/reducers.js
@@ -1,3 +1,77 @@
+// export const ADD_PRODUCT = 'ADD_PRODUCT'
+// export const REMOVE_PRODUCT = 'REMOVE_PRODUCT'
+// export const FETCH_INIT = 'FETCH_INIT'
+// export const FETCH_SUCCESS = 'FETCH_SUCCESS'
+// export const FETCH_FAILURE = 'FETCH_FAILURE'
+
+// const addProductToCart = (product, state) => {
+//   const updatedCart = [...state.cart]
+//   const updatedItemIndex = updatedCart.findIndex(item => item._id === product._id)
+
+//   if (updatedItemIndex < 0) {
+//     updatedCart.push({ ...product, quantity: 1 })
+//   } else {
+//     const updatedItem = {
+//       ...updatedCart[updatedItemIndex],
+//     }
+//     updatedItem.quantity++
+//     updatedCart[updatedItemIndex] = updatedItem
+//   }
+//   return { ...state, cart: updatedCart }
+// }
+
+// const removeProductFromCart = (productId, state) => {
+//   console.log('Removing product with id: ' + productId)
+//   const updatedCart = [...state.cart]
+//   const updatedItemIndex = updatedCart.findIndex(item => item._id === productId)
+
+//   const updatedItem = {
+//     ...updatedCart[updatedItemIndex],
+//   }
+//   updatedItem.quantity--
+//   if (updatedItem.quantity <= 0) {
+//     updatedCart.splice(updatedItemIndex, 1)
+//   } else {
+//     updatedCart[updatedItemIndex] = updatedItem
+//   }
+//   return { ...state, cart: updatedCart }
+// }
+
+// const fetchProducts = (products, state) => {
+//   return { ...state, products }
+// }
+
+// export const shopReducer = (state, action) => {
+//   switch (action.type) {
+//     case FETCH_INIT:
+//       return {
+//         ...state,
+//         isLoading: true,
+//         isError: false,
+//       }
+//     case FETCH_SUCCESS:
+//       // return {
+//       //   ...state,
+//       //   isLoading: false,
+//       //   isError: false,
+//       //   products: action.payload,
+//       //   fetchProducts
+//       // }
+//       return fetchProducts(action.products, state)
+//     case FETCH_FAILURE:
+//       return {
+//         ...state,
+//         isLoading: false,
+//         isError: true,
+//       }
+//     case ADD_PRODUCT:
+//       return addProductToCart(action.product, state)
+//     case REMOVE_PRODUCT:
+//       return removeProductFromCart(action.productId, state)
+//     default:
+//       return state
+//   }
+// }
 export const ADD_PRODUCT = 'ADD_PRODUCT'
 export const REMOVE_PRODUCT = 'REMOVE_PRODUCT'
 export const FETCH_INIT = 'FETCH_INIT'
@@ -17,6 +91,11 @@ const addProductToCart = (product, state) => {
     updatedItem.quantity++
     updatedCart[updatedItemIndex] = updatedItem
   }
+
+  //make cart a string and store in local space
+  let stringCart = JSON.stringify(updatedCart);
+  localStorage.setItem("cart", stringCart)
+
   return { ...state, cart: updatedCart }
 }
 
@@ -24,6 +103,7 @@ const removeProductFromCart = (productId, state) => {
   console.log('Removing product with id: ' + productId)
   const updatedCart = [...state.cart]
   const updatedItemIndex = updatedCart.findIndex(item => item._id === productId)
+  const cart =JSON.parse(localStorage.getItem("cart"));
 
   const updatedItem = {
     ...updatedCart[updatedItemIndex],
@@ -31,9 +111,14 @@ const removeProductFromCart = (productId, state) => {
   updatedItem.quantity--
   if (updatedItem.quantity <= 0) {
     updatedCart.splice(updatedItemIndex, 1)
+    localStorage.setItem("cart", JSON.stringify(cart.splice(updatedItemIndex, 1)));
   } else {
     updatedCart[updatedItemIndex] = updatedItem
   }
+
+  let cartString = JSON.stringify(updatedCart)
+  localStorage.setItem('cart', cartString)
+
   return { ...state, cart: updatedCart }
 }
 
@@ -43,27 +128,26 @@ const fetchProducts = (products, state) => {
 
 export const shopReducer = (state, action) => {
   switch (action.type) {
-    case FETCH_INIT:
-      return {
-        ...state,
-        isLoading: true,
-        isError: false,
-      }
+    // case FETCH_INIT:
+    //   return {
+    //     ...state,
+    //     isLoading: true,
+    //     isError: false,
+    //   }
     case FETCH_SUCCESS:
-      // return {
-      //   ...state,
-      //   isLoading: false,
-      //   isError: false,
-      //   products: action.payload,
-      //   fetchProducts
-      // }
       return fetchProducts(action.products, state)
-    case FETCH_FAILURE:
       return {
         ...state,
         isLoading: false,
-        isError: true,
+        isError: false,
+        products: action.products,
       }
+    // case FETCH_FAILURE:
+    //   return {
+    //     ...state,
+    //     isLoading: false,
+    //     isError: true,
+    //   }
     case ADD_PRODUCT:
       return addProductToCart(action.product, state)
     case REMOVE_PRODUCT:
--- client/src/context/store.js
+++ client/src/context/store.js
@@ -1,3 +1,81 @@
+// import React from 'react'
+// import axios from 'axios'
+
+// import ShopContext from './shop-context'
+// import {
+//   shopReducer,
+//   ADD_PRODUCT,
+//   REMOVE_PRODUCT,
+//   FETCH_INIT,
+//   FETCH_SUCCESS,
+//   FETCH_FAILURE,
+// } from './reducers'
+// import categories from '../component/categories'
+
+// const GlobalState = props => {
+//   const [state, dispatch] = React.useReducer(shopReducer, {
+//     products: [],
+//     cart: [],
+//     isLoading: false,
+//     isError: false,
+//   })
+
+//   const addProductToCart = product => {
+//     setTimeout(() => {
+//       dispatch({ type: ADD_PRODUCT, product: product })
+//     }, 700)
+//   }
+
+//   const removeProductFromCart = productId => {
+//     setTimeout(() => {
+//       dispatch({ type: REMOVE_PRODUCT, productId: productId })
+//     }, 700)
+//   }
+
+//   const fetchProducts = async (token, category) => {
+//     dispatch({ type: FETCH_INIT })
+
+//     try {
+//       const config = {
+//         headers: { Authorization: `Bearer ${token}` },
+//       }
+
+//       const bodyParameters = {
+//         category,
+//       }
+
+//       const result = await axios.post(
+//         `http://localhost:7000/api/categories`,
+//         bodyParameters,
+//         config,
+//       )
+
+//       if (result.status === 201) {
+//         dispatch({ type: FETCH_SUCCESS, products: result.data.data.products })
+//       }
+//     } catch (error) {
+//       dispatch({ type: FETCH_FAILURE })
+//     }
+//   }
+
+//   return (
+//     <ShopContext.Provider
+//       value={{
+//         products: state.products,
+//         cart: state.cart,
+//         isLoading: state.isLoading,
+//         isError: state.isError,
+//         addProductToCart: addProductToCart,
+//         removeProductFromCart: removeProductFromCart,
+//         fetchProducts: fetchProducts,
+//       }}
+//     >
+//       {props.children}
+//     </ShopContext.Provider>
+//   )
+// }
+
+// export default GlobalState
 import React from 'react'
 import axios from 'axios'
 
@@ -10,14 +88,11 @@ import {
   FETCH_SUCCESS,
   FETCH_FAILURE,
 } from './reducers'
-import categories from '../component/categories'
 
 const GlobalState = props => {
   const [state, dispatch] = React.useReducer(shopReducer, {
-    products: [],
     cart: [],
-    isLoading: false,
-    isError: false,
+    products: []
   })
 
   const addProductToCart = product => {
@@ -32,30 +107,47 @@ const GlobalState = props => {
     }, 700)
   }
 
-  const fetchProducts = async (token, category) => {
-    dispatch({ type: FETCH_INIT })
+  const useDataApi = (token, category) => {
 
-    try {
-      const config = {
-        headers: { Authorization: `Bearer ${token}` },
+    React.useEffect(() => {
+      let didCancel = false
+
+      const fetchData = async () => {
+        dispatch({ type: FETCH_INIT })
+
+        try {
+          const config = {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+
+          const bodyParameters = {
+            category,
+          }
+
+          const result = await axios.post(
+            `http://localhost:7000/api/categories`,
+            bodyParameters,
+            config,
+          )
+
+          if (!didCancel && result.status === 201) {
+            dispatch({ type: FETCH_SUCCESS, products: result.data.data.products })
+          }
+        } catch (error) {
+          if (!didCancel) {
+            dispatch({ type: FETCH_FAILURE })
+          }
+        }
       }
 
-      const bodyParameters = {
-        category,
-      }
+      fetchData()
 
-      const result = await axios.post(
-        `http://localhost:7000/api/categories`,
-        bodyParameters,
-        config,
-      )
-
-      if (result.status === 201) {
-        dispatch({ type: FETCH_SUCCESS, products: result.data.data.products })
+      return () => {
+        didCancel = true
       }
-    } catch (error) {
-      dispatch({ type: FETCH_FAILURE })
-    }
+    }, [category])
+
+    return state
   }
 
   return (
@@ -63,11 +155,11 @@ const GlobalState = props => {
       value={{
         products: state.products,
         cart: state.cart,
-        isLoading: state.isLoading,
-        isError: state.isError,
+        // isLoading: state.isLoading,
+        // isError: state.isError,
         addProductToCart: addProductToCart,
         removeProductFromCart: removeProductFromCart,
-        fetchProducts: fetchProducts,
+        useDataApi: useDataApi,
       }}
     >
       {props.children}
